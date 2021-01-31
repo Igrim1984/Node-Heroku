c2541bcf2f5e7d34cbdc3c2d57857b0f34f624dc
Igrim1984 <lukasz.czolczynski84@gmail.com>
2021-01-31 13:34:48 +0100

    initial

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const port = proces.any.PORT || 3000;
+const app = express();
+//ustawienie, ze moja aplikacja musi korzystac z silnika hbs
+app.set("view engine", 'hbs')
+// Podpięcie css
+const path = require('path')
+app.use('/assets', express.static(path.join(__dirname, "./assets")));
+app.use('/JS', express.static(path.join(__dirname, "./js")));
+//gdy uzytkownik wchodzi na stronę
+app.get('/', function (req, res) {
+    res.render('index',
+    {pageTitle: "Lekcja 01"})
+})
+app.get('/about', function (req, res) {
+    res.send("My site")
+})
+app.listen(port, (err) => {
+    if (err) {
+        return console.log("coś poszło nie tak...:", err)
+    }
+    console.log("serwer działa na porcie", port)
+})
---
+++ functions.js
@@ -0,0 +1,9 @@
+math = (a,b) => console.log(2*a + b)
+
+
+
+module.exports = {
+    math
+}
+
+console.log('załadowano wszystkie funkcje')
